d5a740ea84e0f6d0b9566a998f73a525feaf0abd
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-06-03 14:49:20 +0200

    Get vpk list: follow symlinks

--- src/server/api.go
+++ src/server/api.go
@@ -57,10 +57,9 @@ func apiGetVpkList(c *gin.Context) apiError {
 	root := "vpk/"
 	//files, err = FilePathWalkDir(root)
 	var files []string
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+	err := Walk(root, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() && strings.HasSuffix(path, "_dir.vpk") {
 			//files = append(files, strings.TrimSuffix(info.Name(), "_dir.vpk"))
-			//log.Println(filepath.Dir(path))
 			//dir, file := filepath.Split(strings.TrimPrefix(path, root))
 			//fmt.Printf("input: %q\n\tdir: %q\n\tfile: %q\n", path, dir, file)
 			files = append(files, strings.TrimPrefix(strings.TrimSuffix(path, "_dir.vpk"), root))
@@ -82,4 +81,46 @@ func apiGetVpkList(c *gin.Context) apiError {
 
 	jsonSuccess(c, map[string]any{"files": files})
 	return nil
+}
+
+// symwalkFunc calls the provided WalkFn for regular files.
+// However, when it encounters a symbolic link, it resolves the link fully using the
+// filepath.EvalSymlinks function and recursively calls symwalk.Walk on the resolved path.
+// This ensures that unlink filepath.Walk, traversal does not stop at symbolic links.
+func walk(filename string, linkDirname string, walkFn filepath.WalkFunc, visitedDirs map[string]struct{}) error {
+	symWalkFunc := func(path string, info os.FileInfo, err error) error {
+
+		if fname, err := filepath.Rel(filename, path); err == nil {
+			path = filepath.Join(linkDirname, fname)
+		} else {
+			return err
+		}
+
+		if err == nil && info.Mode()&os.ModeSymlink == os.ModeSymlink {
+			finalPath, err := filepath.EvalSymlinks(path)
+			if err != nil {
+				return err
+			}
+			info, err := os.Lstat(finalPath)
+			if err != nil {
+				return walkFn(path, info, err)
+			}
+			if info.IsDir() {
+				if _, ok := visitedDirs[finalPath]; ok {
+					return filepath.SkipDir
+				}
+				visitedDirs[finalPath] = struct{}{}
+
+				return walk(finalPath, path, walkFn, visitedDirs)
+			}
+		}
+
+		return walkFn(path, info, err)
+	}
+	return filepath.Walk(filename, symWalkFunc)
+}
+
+// Walk extends filepath.Walk to also follow symlinks
+func Walk(path string, walkFn filepath.WalkFunc) error {
+	return walk(path, path, walkFn, make(map[string]struct{}))
 }
