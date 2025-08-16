81ef12d07d0adbded90cd7708a3e2b0333215686
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-08-16 16:50:53 +0200

    Material viewer: added source 2 textures

--- src/client/ts/view/materialviewer.ts
+++ src/client/ts/view/materialviewer.ts
@@ -1,6 +1,6 @@
 import { vec3, vec4 } from 'gl-matrix';
 import { AmbientLight, Camera, ColorBackground, ContextObserver, GraphicsEvents, OrbitControl, Plane, RenderFace, Scene, Source2Material, Source2MaterialManager } from 'harmony-3d';
-import { downloadSVG } from 'harmony-svg';
+import { downloadSVG, openInNewSVG } from 'harmony-svg';
 import { createElement, createShadowRoot, hide, show } from 'harmony-ui';
 import { Map2 } from 'harmony-utils';
 import materialViewerCSS from '../../css/materialviewer.css';
@@ -9,6 +9,18 @@ import { ControllerEvents, SelectFile } from '../controllerevents';
 import { setParent, setScene, startupRenderer } from '../graphics';
 import { SiteElement } from './siteelement';
 
+const textureName = new Map<string, string>([
+	['g_tColor', '#color'],
+	['g_tMasks1', '#masks1'],
+	['g_tMasks2', '#masks2'],
+	['g_tDetail', '#detail1'],
+	['g_tDetail2', '#detail2'],
+	['g_tFresnelWarp', '#fresnel_waarp'],
+	['g_tNormal', '#normal'],
+	['g_tCubeMap', '#cube_map'],
+]);
+
+
 export class MaterialViewer extends SiteElement {
 	#htmlToolbar?: HTMLElement;
 	#htmlParams?: HTMLElement;
@@ -16,6 +28,7 @@ export class MaterialViewer extends SiteElement {
 	#htmlFloatParams?: HTMLElement;
 	#htmlVectorParams?: HTMLElement;
 	#htmlDynamicParams?: HTMLElement;
+	#htmlTextures?: HTMLElement;
 	#html3d?: HTMLElement;
 	#repository: string = '';
 	#path: string = '';
@@ -46,10 +59,11 @@ export class MaterialViewer extends SiteElement {
 						this.#htmlParams = createElement('div', {
 							class: 'params',
 							childs: [
-								this.#htmlIntParams = createElement('div', { class: 'params', }),
-								this.#htmlFloatParams = createElement('div', { class: 'params', }),
-								this.#htmlVectorParams = createElement('div', { class: 'params', }),
-								this.#htmlDynamicParams = createElement('div', { class: 'params', }),
+								this.#htmlIntParams = createElement('div', { class: 'variables', }),
+								this.#htmlFloatParams = createElement('div', { class: 'variables', }),
+								this.#htmlVectorParams = createElement('div', { class: 'variables', }),
+								this.#htmlDynamicParams = createElement('div', { class: 'variables', }),
+								this.#htmlTextures = createElement('div', { class: 'variables', }),
 							]
 						}),
 						this.#html3d = createElement('div', {
@@ -117,44 +131,77 @@ export class MaterialViewer extends SiteElement {
 		this.#updateParam(this.#htmlFloatParams!, material.getFloatParams(), 'number');
 		this.#updateParam(this.#htmlVectorParams!, material.getVectorParams(), 'vec4');
 		this.#updateParam(this.#htmlDynamicParams!, material.getDynamicParams(), 'expression');
+		this.#updateParam(this.#htmlTextures!, material.getTextureParams(), 'texture');
 	}
 
-	#updateParam(element: HTMLElement, params: Map<string, number> | Map<string, vec4> | Map<string, [string | null, Uint8Array]> | null, type: string): void {
+	#updateParam(element: HTMLElement, params: Map<string, number> | Map<string, vec4> | Map<string, string> | Map<string, [string | null, Uint8Array]> | null, type: string): void {
 		element.replaceChildren();
 		if (params && params.size) {
 			show(element);
 			for (const [name, param] of params) {
 				let value: HTMLElement;
-				createElement('label', {
-					class: 'param',
-					parent: element,
-					childs: [
-						createElement('span', { innerText: name }),
-						value = createElement('span'),
-					],
-				});
 
 				switch (type) {
 					case 'number':
 					case 'string':
-						value.innerText = String(param);
+						createElement('label', {
+							class: 'param',
+							parent: element,
+							childs: [
+								createElement('span', { innerText: name }),
+								createElement('span', { innerText: String(param) }),
+							],
+						});
 						break;
 					case 'expression':
-						value.classList.add('expression')
 						const text = (param as [string | null, Uint8Array])[0] ?? 'error while decompiling expression';//TODO: i18n
 						const rows = (text.match(/,/g) || []).length;
-						createElement('textarea', {
-							parent: value,
-							rows: rows,
-							cols: 60,
-							disabled: true,
-							properties: {
-								value: text,
-							},
+						createElement('div', {
+							class: 'variable',
+							parent: element,
+							childs: [
+								createElement('span', { innerText: name }),
+								createElement('textarea', {
+									rows: rows,
+									cols: 60,
+									disabled: true,
+									properties: {
+										value: text,
+									},
+								}),
+							],
+						});
+						;
+						break;
+					case 'texture':
+						const path = (param as string).replace(/\.vtex_c$/, '').replace(/\.vtex$/, '') + '.vtex_c';
+						createElement('div', {
+							parent: element,
+							class: 'variable',
+							childs: [
+								createElement('div', {
+									class: 'title',
+									i18n: textureName.get(name) ?? name,
+								}),
+								createElement('div', {
+									class: 'path',
+									childs: [
+										createElement('input', {
+											disabled: true,
+											value: param as string,
+										}),
+										createElement('span', {
+											class: 'open',
+											innerHTML: openInNewSVG,
+
+											$click: () => Controller.dispatchEvent(new CustomEvent<SelectFile>(ControllerEvents.SelectFile, { detail: { repository: this.#repository, path: path } })),
+										}),
+									],
+								}),
+							]
 						});
 						break;
 				}
-
 			}
 		} else {
 			hide(element);
